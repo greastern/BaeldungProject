1ababfa27a8869888739a6d3ae10afa5736c99c3
gaius_6pcksas <gaiusroy@gmail.com>
2021-09-12 12:59:01 -0400

    Security Config upload.

--- src/main/java/SecurityConfig.java
+++ src/main/java/SecurityConfig.java
@@ -2,7 +2,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfigurerAdapter {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
