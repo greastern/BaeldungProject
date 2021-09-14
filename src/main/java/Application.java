d5bc4dfbd606e3bec3f949ba2dc041f381f81cc2
gaius_6pcksas <gaiusroy@gmail.com>
2021-09-14 18:47:49 -0400

    configure new persistence layer in the main: @EnableJpaRepositories("com.baeldung.persistence.repo") @EntityScan("com.baeldung.persistence.model")

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,6 +1,10 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories("com.baeldung.persistence.repo")
+@EntityScan("com.baeldung.persistence.model")
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
